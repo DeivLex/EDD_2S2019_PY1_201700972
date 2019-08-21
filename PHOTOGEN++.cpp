e99e7e0bc51ab4d4f2db2fbe302e0dc5877a54ee
DeivLex <peloliso.98@gmail.com>
2019-08-21 12:41:09 -0600

    Avances

--- PHOTOGEN++.cpp
+++ PHOTOGEN++.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <stdlib.h>
-
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 using namespace std;
+
+string nombre;
+string html;
+string css;
+
+string ancho_imagen;
+string alto_imagen;
+string ancho_pixel;
+string alto_pixel;
+void InsertarImagen(string nombre);
+void AbrirConfig(string config);
+void EscribirHtml(string html,string css);
+void Escribirscss(string css,string ancho_imagen,string alto_imagen,string ancho_pixel,string alto_pixel);
 
 int main ()
 {
@@ -22,10 +38,15 @@ int main ()
 		switch (a)
 		{
 			case 1:
-				cout<< "Opcion1\n";
+				cout<<"Nombre del archivo: "; 
+				cin>>nombre;
+				InsertarImagen(nombre);
 				break;
 			case 2:
-				cout<< "Opcion2\n";
+				html="hola.html";
+				css ="hola.scss";
+				Escribirscss(css,ancho_imagen,alto_imagen,ancho_pixel,alto_pixel);
+				EscribirHtml(html,css);
 				break;
 			case 3:
 				cout<< "Opcion3\n";
@@ -40,6 +61,7 @@ int main ()
 				cout<< "Opcion6\n";
 				break;				
 			case 7:
+				system("pause");
 				return 0;
 				break;
 			default:
@@ -50,3 +72,143 @@ int main ()
 	while(a != 3);
 	return main();
 }
+void InsertarImagen(string nombre){
+	string config;
+	ifstream infile(nombre.c_str());
+	string line = "";
+	vector<string> all_words;
+	while (getline(infile, line)){
+		stringstream strstr(line);
+		string word = "";
+	while (getline(strstr,word, ';')){
+		all_words.push_back(word);
+	}
+	}
+	for (unsigned i = 0; i < all_words.size(); i++){
+		cout << all_words.at(i)<< "\n";
+		if (all_words.at(i) == "0"){
+			int n = i+1;
+			config = all_words.at(n);
+		}
+	    
+	}
+	AbrirConfig(config);
+	cout<<"\nImagen insertada\n";
+}
+
+void AbrirConfig(string config){
+	ifstream infile(config.c_str());
+	string line = "";
+	vector<string> all_words;
+	while (getline(infile, line)){
+		stringstream strstr(line);
+		string word = "";
+	while (getline(strstr,word, ';')){
+		all_words.push_back(word);
+	}
+	}
+	for (unsigned i = 0; i < all_words.size(); i++){
+		cout << all_words.at(i)<< "\n";
+		if(all_words.at(i)=="image_width"){
+			ancho_imagen=all_words.at(i+1);
+		}else if(all_words.at(i)=="image_height"){
+			alto_imagen=all_words.at(i+1);
+		}else if(all_words.at(i)=="pixel_width"){
+			ancho_pixel=all_words.at(i+1);
+		}else if(all_words.at(i)=="pixel_height"){
+			alto_pixel=all_words.at(i+1);
+		}
+	}
+}
+
+void Escribirscss(string css,string ancho_imagen,string alto_imagen,string ancho_pixel,string alto_pixel){
+	
+	int altoactual = atoi(alto_imagen.c_str())*atoi(alto_pixel.c_str());
+	int anchoactual = atoi(ancho_imagen.c_str())*atoi(ancho_pixel.c_str());
+	
+	ofstream archivo;
+	string nombreArchivo,frase;
+	char rpt;
+	
+	archivo.open(css.c_str(),ios::out); //Creamos el archivo
+	
+	if(archivo.fail()){ //Si a ocurrido algun error
+		cout<<"No se pudo abrir el archivo";
+		exit(1);
+	}
+
+	archivo<<"body {";
+    archivo<<"background: #333333;";      
+    archivo<<"height: 100vh;";
+    archivo<<"display: flex;";          
+    archivo<<"justify-content: center;";
+    archivo<<"align-items: center;";
+    archivo<<"}";
+
+    archivo<<".canvas {";
+    archivo<<"width: "<< <<"px;";
+    archivo<<"height: "<< <<"px;";
+	archivo<<"}";
+
+	archivo<<".pixel {";
+    archivo<<"width: "<<ancho_imagen<<"px;";
+    archivo<<"height: "<<alto_imagen<<"px;";
+    archivo<<"float: left;";
+    archivo<<"}";
+
+	archivo<<".pixel:nth-child(0),";
+	archivo<<".pixel:nth-child(9),";
+	archivo<<".pixel:nth-child(10),";
+	archivo<<".pixel:nth-child(14),";
+	archivo<<".pixel:nth-child(15),";
+	archivo<<".pixel:nth-child(16),";
+	archivo<<".pixel:nth-child(17),";
+	archivo<<".pixel:nth-child(20),";
+	archivo<<".pixel:nth-child(21),";
+	archivo<<".pixel:nth-child(22),";
+	archivo<<".pixel:nth-child(23),";
+	archivo<<".pixel:nth-child(27),";
+	archivo<<".pixel:nth-child(28){";
+    archivo<<"background: #000000;";
+	archivo<<"}";
+
+	archivo.close(); //Cerramos el archivo
+}
+
+void EscribirHtml(string html, string css){
+	ofstream archivo;
+	string nombreArchivo,frase;
+	char rpt;
+	
+	archivo.open(html.c_str(),ios::out); //Creamos el archivo
+	
+	if(archivo.fail()){ //Si a ocurrido algun error
+		cout<<"No se pudo abrir el archivo";
+		exit(1);
+	}
+
+	archivo<<"<!DOCTYPE html>"<<endl;
+	archivo<<"<html>"<<endl;
+	archivo<<"<head>"<<endl;
+    archivo<<"<link rel="<<(char)34<<"stylesheet"<<(char)34<<" href="<<(char)34<<css.c_str()<<(char)34<<">"<<endl;
+    archivo<<"</head>"<<endl;
+    archivo<<"<body>"<<endl;
+    archivo<<"<div class="<<(char)34<<"canvas"<<(char)34<<">"<<endl;
+    
+    for(int i = 0; i < 36; i++){
+    archivo<<"<div class="<<(char)34<<"pixel"<<(char)34<<"></div>"<<endl;
+	}
+    archivo<<"</div>"<<endl;
+	archivo<<"</body>"<<endl;
+	archivo<<"</html>"<<endl;
+
+	archivo.close(); //Cerramos el archivo
+}
+
+
+
+
+
+
+
+
